dabc8451cc0f92621b918bc56df1f85565c99803
eduard_nicolae <eduard_nicolae@yahoo.com>
2022-03-05 17:26:49 +0200

    Server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('./dist/proman'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/proman/' }),
+);
+
+app.listen(process.env.PORT || 8080);
