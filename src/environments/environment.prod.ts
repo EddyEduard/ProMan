7aa4030a9f51bce2028827a7619bb558ff045be8
eduard_nicolae <eduard_nicolae@yahoo.com>
2022-03-07 12:15:51 +0200

    Env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://localhost:8080/api",
+  // apiUrl: "http://localhost:8080/api",
+  apiUrl: "https://proman-rest.herokuapp.com/api",
   aesKey: "ProManSecretKey"
 };
